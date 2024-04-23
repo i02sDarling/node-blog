e6222284da0266cbe82e94fba64778fb87b49d0e
jiuyue1368458 <2668933894@qq.com>
2024-04-23 13:10:06 +0800

    feat:icon

--- app.js
+++ app.js
@@ -2,35 +2,50 @@ const path = require('path');
 const fs = require('fs');
 const koa = require('koa');
 const mount = require('koa-mount');
-const static = require('koa-static');
-
+const serveStatic = require('koa-static');
+const staticPath='./template/static'
+const template = require('./template/template.js')(path.join(__dirname + '/template/template.html'))
 const app = new koa();
-
+console.log(path.join(__dirname, staticPath))
 app.use(
-    static(path.join(__dirname, '/source/'))
+    serveStatic(path.join(__dirname, staticPath))
 );
 
 app.use(
-    mount('/resume',static(path.join(__dirname, 'msg')))
+    mount('/resume',serveStatic(path.join(__dirname, staticPath,'resume.pdf')))
 );
 
 app.use(async (ctx, next) => {
     if (ctx.url === '/resume') {
         ctx.type = 'application/pdf';
-        ctx.body = fs.readFileSync(path.join(__dirname, 'msg/resume.pdf'));
+        ctx.body = fs.readFileSync(path.join(__dirname, staticPath,'resume.pdf'));
     } else {
         await next();
     }
 });
 
 app.use(
+
     mount('/', async (ctx) => {
         ctx.status = 200;
-        const filePath = path.join(__dirname, 'msg/template.html');
-        ctx.body = fs.readFileSync(filePath, 'utf-8');
+        const filePath = path.join(__dirname, 'template','template.html');
+        ctx.body = template({
+            Title1:"hh",
+            Data1:'2023-1-2',
+            TagF:'Note',
+            URL1:'https://nodeblog-d2hk--3000--7dbe22a9.local-corp.webcontainer.io/article?page=1&id=1',
+            
+            Title2:'hh2',
+            Data2:'2023-1-2',
+            Tag2:'Note',
+            URL1:'https://nodeblog-d2hk--3000--7dbe22a9.local-corp.webcontainer.io/article?page=1&id=2',
+
+            
+
+        })
         
     })
 );
 
-
+//fetch(`./data?sort=${this.state.sortType}&filt=${filtType}`)
 app.listen(3000);
---
+++ data/mockdata.js
@@ -0,0 +1,14 @@
+module.exports=[
+    {
+        id:1,
+        title:'',
+        date:'',
+        
+    },
+    {},
+    {},
+    {
+
+    }
+
+]
