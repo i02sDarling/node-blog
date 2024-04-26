82b308fa76c60141eef620a32f03c08af3d06202
袁哲 <2668933894@qq.com>
2024-04-26 15:25:41 +0800

    feat:createNewLocalBranch

--- app.js
+++ app.js
@@ -3,37 +3,39 @@ const fs = require('fs');
 const koa = require('koa');
 const mount = require('koa-mount');
 const serveStatic = require('koa-static');
-const staticPath='./template/static'
+const staticPath = './template/static'
 const app = new koa();
-const getPage=require('./data/getPage.js')
-const getArticle=require('./data/getArticleMsg.js')
-const Template=require('./template/template.js')
-const articleTemplate=require('./template/template.js')(path.join(staticPath,'article.html'))
-const HomePage=require('./template/Home.js');
-const getData=require('./request/get-data.js')
+const getPage = require('./data/getPage.js')
+const getArticle = require('./data/getArticleMsg.js')
+const Template = require('./template/template.js')
+const articleTemplate = require('./template/template.js')(path.join(staticPath, 'article.html'))
+const HomePage = require('./template/Home.js');
+const getData = require('./request/get-data.js')
 // const convert=require('./data/converMdToHtml.js')
 
 // await getData(+(ctx.query.sort || 0), +(ctx.query.filt || 0));
 
+app.use(mount('/data', async (ctx) => {
+    ctx.body = await getData(+(ctx.query.sort || 0), +(ctx.query.filt || 0));
+}));
 
-
-const header=fs.readFileSync(path.join(staticPath,'/header.txt'),'utf-8');
+const header = fs.readFileSync(path.join(staticPath, '/header.txt'), 'utf-8');
 
 app.use(
     serveStatic(path.join(__dirname, staticPath))
 );
 
 app.use(
-    mount('/resume',serveStatic(path.join(__dirname, staticPath,'Resume-index.html')))
+    mount('/resume', serveStatic(path.join(__dirname, staticPath, 'Resume-index.html')))
 );
 app.use(
-    mount('/svg',serveStatic(path.join(__dirname, staticPath,'Svg-nodejs.html')))
+    mount('/svg', serveStatic(path.join(__dirname, staticPath, 'Svg-nodejs.html')))
 );
 app.use(
-    mount('/day',serveStatic(path.join(__dirname, staticPath,'Day-index.html')))
+    mount('/day', serveStatic(path.join(__dirname, staticPath, 'Day-index.html')))
 );
 app.use(
-    mount('/404',serveStatic(path.join(__dirname, staticPath,'404-index.html')))
+    mount('/404', serveStatic(path.join(__dirname, staticPath, '404-index.html')))
 );
 // app.use(
 //     mount('/day',serveStatic(path.join(__dirname, staticPath,'article2')))
@@ -42,31 +44,31 @@ app.use(
 app.use(async (ctx, next) => {
     if (ctx.url === '/resume') {
         // ctx.body = fs.readFileSync(path.join(__dirname, staticPath,'resume.pdf'));
-        ctx.body=fs.readFileSync(path.join(__dirname,staticPath,'Resume-index.html'),'utf-8');
-    } else if(ctx.url==='/svg'){
-        ctx.body=fs.readFileSync(path.join(__dirname,staticPath,'Svg-nodejs.html'),'utf-8')
+        ctx.body = fs.readFileSync(path.join(__dirname, staticPath, 'Resume-index.html'), 'utf-8');
+    } else if (ctx.url === '/svg') {
+        ctx.body = fs.readFileSync(path.join(__dirname, staticPath, 'Svg-nodejs.html'), 'utf-8')
     }
-    else if(ctx.url==='/day'){
-        ctx.body=fs.readFileSync(path.join(__dirname,staticPath,'Day-index.html'),'utf-8')
+    else if (ctx.url === '/day') {
+        ctx.body = fs.readFileSync(path.join(__dirname, staticPath, 'Day-index.html'), 'utf-8')
     }
-    else if(ctx.url==='/404'){
-        ctx.body=fs.readFileSync(path.join(__dirname,staticPath,'404-index.html'),'utf-8')
+    else if (ctx.url === '/404') {
+        ctx.body = fs.readFileSync(path.join(__dirname, staticPath, '404-index.html'), 'utf-8')
     }
-    else{
+    else {
         await next();
     }
 });
- 
+
 app.use(
-    mount('/article',async(ctx,next)=>{
-        req=+(ctx.query.articleId||0)
-        if(req===0){
+    mount('/article', async (ctx, next) => {
+        req = +(ctx.query.articleId || 0)
+        if (req === 0) {
             next();
         }
-        else{
-            article=getArticle(req);
-            component_header=header;
-            ctx.body=Template(path.join(staticPath,'article.html'))({article,component_header});
+        else {
+            article = getArticle(req);
+            component_header = header;
+            ctx.body = Template(path.join(staticPath, 'article.html'))({ article, component_header });
         }
 
     })
@@ -76,24 +78,24 @@ app.use(
 
     mount('/', async (ctx) => {
         ctx.status = 200;
-        pageNum=ctx.query.page?ctx.query.page:0;
-        if(pageNum===0){
-            ctx.body=HomePage();
-            
-        }else{
+        pageNum = ctx.query.page ? ctx.query.page : 0;
+        if (pageNum === 0) {
+            ctx.body = HomePage();
+
+        } else {
             const templateParams = {};
-            pageInfo=getPage(pageNum);
+            pageInfo = getPage(pageNum);
             pageInfo.pages.map((data, index) => {
-                
+
                 const num = index + 1;
                 templateParams[`Title${num}`] = data.title;
                 templateParams[`Date${num}`] = data.date;
                 templateParams[`Tag${num}`] = 'note';
-                templateParams[`URL${num}`] = '/article?articleId='+data.id;
-            }); 
-            templateParams[`component_header`]=header;
-            templateParams[`nextPageURL`]=pageInfo.nextUrl;
-            ctx.body =Template(path.join(staticPath,'template.html'))(templateParams);
+                templateParams[`URL${num}`] = '/article?articleId=' + data.id;
+            });
+            templateParams[`component_header`] = header;
+            templateParams[`nextPageURL`] = pageInfo.nextUrl;
+            ctx.body = Template(path.join(staticPath, 'template.html'))(templateParams);
         }
     })
 );
